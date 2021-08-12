ececb5f27d2d6c73b7e29c270b83a0213989a2ea
Ademir <ademir.m.junior@gmail.com>
2021-08-12 11:18:57 -0300

    Develop action for implicit Intent

--- app/src/main/java/br/edu/utfpr/callactivitywithimplicitintent/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/callactivitywithimplicitintent/MainActivity.java
@@ -2,7 +2,11 @@ package br.edu.utfpr.callactivitywithimplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openUtfprWebsite(View view) {
+        openWebsite("http://www.utfpr.edu.br");
+    }
+
+    private void openWebsite( String address) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        intent.setData(Uri.parse(address));
+
+        intent.resolveActivity(getPackageManager());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this,
+                    R.string.there_is_no_app_to_run_this_action,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
